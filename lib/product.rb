955f0820354c6457cee1b6dc325f077d5944021a
Geomas80 <masourasgeorge@gmail.com>
2016-05-01 21:34:10 +0300

    Finished with Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,13 @@
 class Product
 
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 
 	def initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products
 	end
 
@@ -14,7 +16,20 @@ class Product
 	end
 
 	def self.find_by_title(product_title)
+		@@products.each do |product|
+			if product.title == product_title
+				return product
+			end
+		end
+		puts "There is no such a product with that title: #{product_title}"
+	end
 
+	def in_stock?
+		@stock > 0		
+	end
+
+	def self.in_stock
+		@@products.select {|product| product.stock > 0}
 	end
 
 	private
