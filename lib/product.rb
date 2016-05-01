0840eb3446fde5b7db6042e2a595062847f24806
Geomas80 <masourasgeorge@gmail.com>
2016-05-01 04:42:28 +0300

    Exception for duplicate products completed

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,31 @@
 class Product
 
+	attr_reader :title
+
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(product_title)
+
+	end
+
+	private
+
+	def add_to_products
+		products_titles = @@products.map {|product| product.title}
+		if products_titles.include? @title
+			raise DuplicateProductError, "#{@title} already exists"
+		else
+			@@products << self
+		end
 	end
 
 end
